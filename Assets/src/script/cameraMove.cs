0371b0c656fbaeb578a6541dba447822b7c86820
sechiyo97 <sechiyo97@gmail.com>
2019-07-02 05:59:07 +0900

    멀티터치 줌인&줌아웃 구현

---
+++ Assets/src/script/cameraMove.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour {
+
+    const int MAX_X = 5;
+    const int MIN_X = -5;
+    const int MAX_Y = 5;
+    const int MIN_Y = -5;
+    const float fscale = 0.01f;
+    const float zoomSpeed = 0.03f;
+
+    Camera cam;
+
+    Vector2 vscale = new Vector2(fscale, fscale);
+    Vector2 startPos;
+    Vector2 curPos;
+    Vector2 change;
+    bool hold = false;
+    
+
+    // Start is called before the first frame update
+    void Start() {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        // swipe
+        if (Input.touchCount == 1) {
+            if (Input.GetMouseButtonDown(0)) {
+                this.hold = true;
+                this.startPos = Input.mousePosition;
+            } else if (Input.GetMouseButtonUp(0)) {
+                this.hold = false;
+            }
+            if (this.hold) {
+                this.curPos = Input.mousePosition;
+                transform.Translate((cam.orthographicSize / 5) * vscale * (startPos - curPos));
+                if (outside()) transform.Translate((cam.orthographicSize/5) * vscale * (curPos - startPos));
+                startPos = curPos;
+            }
+        }
+
+        // zoom
+        else if (Input.touchCount > 1) {
+            // store both touches
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // check position
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // check deltas (original current)
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // check how much zoom in / out
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            // zoom camera
+            float newSize = cam.orthographicSize + (deltaMagnitudeDiff * zoomSpeed);
+            newSize = Mathf.Max(newSize, 1f);
+            newSize = Mathf.Min(newSize, 6f);
+            cam.orthographicSize = newSize;
+
+        }
+    }
+
+    // check if outside the boundary
+    bool outside() {
+        if (transform.position.x > MAX_X || transform.position.x < MIN_X
+            || transform.position.y > MAX_Y || transform.position.y < MIN_Y)
+            return true;
+        else return false;
+    }
+}
--- Assets/src/script/catMove.cs
+++ Assets/src/script/catMove.cs
@@ -1,39 +1,53 @@
 ﻿using UnityEngine;
 
 public class catMove : MonoBehaviour {
-    float[,] chairPos
+    float[,] chairOnPos
         = { { 3.78f, -0.44f }, { 1.84f, -1.45f }, { 0.12f, -2.44f }, { -1.8f, -1.38f }
         , { -3.84f, -0.81f }, { -1.91f, 0.3f }, { -0.5f, -0.37f }, { 1.26f, -0.37f } , { 1.45f, 0.72f }
         , { -3.95f, 2.33f }, { -5.85f, 3.41f }, { -2.04f, 3.49f }, { -3.9f, 4.57f }  }; // 수정 필요
+    float[,] chairOffPos
+        = { { 2.98f, -0.31f }, { 1.84f, -1.45f }, { 0.12f, -2.44f }, { -1.8f, -1.38f }
+        , { -3.84f, -0.81f }, { -1.91f, 0.3f }, { -0.5f, -0.37f }, { 1.26f, -0.37f } , { 1.45f, 0.72f }
+        , { -3.95f, 2.33f }, { -5.85f, 3.41f }, { -2.04f, 3.49f }, { -3.9f, 4.57f }  }; // 수정 필요ssssssssssssssssssssssssssssssssssssssssssssssssssssss
     int[] chairType
         = { 1, 3, 4, 2, 3, 1, 3, 4, 1, 4, 2, 4, 2 };
     int[] catLayer
         = {25,29,39,35, 9,5,19,19,15,17,13,7,3};
     int chairNum;
 
-    public bool drink = false;
-    public bool walking = true;
+    // 4 status of cat: coming, waiting, eating, going
+    public int status = 1;
+
+    bool arrive = false;
     bool forward = true;
     Animator animator;
     GameObject gameDirector;
-new SpriteRenderer renderer;
+    new SpriteRenderer renderer;
 
     // Start is called before the first frame update
     void Start() {
         this.gameDirector = GameObject.Find("gameDirector");
         this.animator = GetComponent<Animator>();
         this.renderer = GetComponent<SpriteRenderer>();
-        this.chairNum = 5;// Random.Range(1,9);
+        this.chairNum = 1;// Random.Range(1,9);
         while(gameDirector.GetComponent<gameDirector>().isOccupied[chairNum] == true) this.chairNum = Random.Range(1,9);
         gameDirector.GetComponent<gameDirector>().isOccupied[chairNum] = true;
     }
 
     // Update is called once per frame
     void Update() {
+        switch (status) {
+            case 1: coming(); break;
+            case 2: waiting(); break;
+            case 3: eating(); break;
+            case 4: going(); break;
+        }
+    }
 
-        if (drink && walking) {
-            walking = false;
-            switch (chairType[chairNum-1]) {
+    // coming to a seat
+    void coming() {
+        if (arrive) { // if sit down, go to next state(2, waiting)
+            switch (chairType[chairNum - 1]) {
                 case 1:
                     animator.SetTrigger("drink_front");
                     renderer.flipX = false;
@@ -51,83 +65,170 @@ new SpriteRenderer renderer;
                     animator.SetTrigger("drink_back");
                     break;
             }
-        } 
-        else if (walking) {
-            switch (chairNum) { // start pos = 1.8, 2.3
+            sitDown(1.0f, catLayer[chairNum - 1]);
+            Invoke("startWaiting", 0);
+            return;
+        } else { // else, move to a seat
+            switch (chairNum) { // start pos = 1.6, 2.2
                 case 1:
                     if (transform.position.y > 1.5f) walk(1, -1, 0);
-                    else if (transform.position.y > -0.1f) walk(1, 1, 0);
+                    else if (transform.position.y > -0.1f) walk(1, 1, 2);
                     else if (transform.position.y > -0.4f) walk(1, -1, 21);
                     else if (transform.position.y > -0.5f) walk(1, 1, 21);
-                    else drink = true;
+                    else arrive = true;
                     break;
                 case 2:
                     if (transform.position.y > 1.5f) walk(1, -1, 0);
-                    else if (transform.position.y > -0.2f) walk(1, 1, 0);
+                    else if (transform.position.y > -0.2f) walk(1, 1, 2);
                     else if (transform.position.y > -1.4f) walk(1, -1, 21);
                     else if (transform.position.y > -1.5f) walk(1, 1, 21);
-                    else drink = true;
+                    else arrive = true;
                     break;
                 case 3:
                     if (transform.position.y > 1.5f) walk(1, -1, 0);
-                    else if (transform.position.y > -0.2f) walk(1, 1, 0);
+                    else if (transform.position.y > -0.2f) walk(1, 1, 2);
                     else if (transform.position.y > -1.9f) walk(1, -1, 21);
                     else if (transform.position.y > -2.2f) walk(1, 1, 31);
                     else if (transform.position.y > -2.3f) walk(1, -1, 31);
-                    else drink = true;
+                    else arrive = true;
                     break;
                 case 4:
                     if (transform.position.y > 0.6f) walk(1, -1, 0);
-                    else if (transform.position.y > 0f) walk(1, 1, 0);
+                    else if (transform.position.y > 0f) walk(1, 1, 2);
                     else if (transform.position.y > -0.6f) walk(1, -1, 11);
                     else if (transform.position.y > -1.2f) walk(1, 1, 31);
                     else if (transform.position.y > -1.3f) walk(1, -1, 31);
-                    else drink = true;
+                    else arrive = true;
                     break;
                 case 5:
                     if (forward) {
                         if (transform.position.y > 0.5f) walk(1, -1, 0);
-                        else if (transform.position.y > -0.1f) walk(1, 1, 0);
+                        else if (transform.position.y > -0.1f) walk(1, 1, 2);
                         else if (transform.position.y > -0.7f) walk(1, -1, 11);
                         else if (transform.position.y > -0.9f) walk(1, 1, 21);
                         else if (transform.position.y > -1.6f) walk(1, -1, 21);
                         else forward = false;
                     } else if (transform.position.y < -1.5f) walk(-1, -1, 11);
-                    else drink = true;
+                    else arrive = true;
                     break;
                 case 6:
                     if (forward) {
                         if (transform.position.y > 0.5f) walk(1, -1, 0);
-                        else if (transform.position.y > -0.1f) walk(1, 1, 0);
+                        else if (transform.position.y > -0.1f) walk(1, 1, 2);
                         else if (transform.position.y > -0.5f) walk(1, -1, 11);
                         else forward = false;
                     } else if (transform.position.y < -0.4f) walk(-1, -1, 11);
-                    else drink = true;
+                    else arrive = true;
                     break;
                 case 7:
                     if (transform.position.y > 0.5f) walk(1, -1, 0);
-                    else if (transform.position.y > 0f) walk(1, 1, 0);
-                    else if (transform.position.y > -0.2f) walk(1, -1, 0);
-                    else if (transform.position.y > -0.3f) walk(1, 1, 0);
-                    else drink = true;
+                    else if (transform.position.y > 0f) walk(1, 1, 2);
+                    else if (transform.position.y > -0.2f) walk(1, -1, 2);
+                    else if (transform.position.y > -0.3f) walk(1, 1, 2);
+                    else arrive = true;
                     break;
                 case 8:
                     if (transform.position.y > 1.5f) walk(1, -1, 0);
-                    else if (transform.position.y > 0.2f) walk(1, 1, 0);
+                    else if (transform.position.y > 0.2f) walk(1, 1, 2);
                     else if (transform.position.y > -0.4f) walk(1, -1, 15);
-                    else drink = true;
+                    else arrive = true;
+                    break;
+                case 9:
+                    if (transform.position.y > 1.1f) walk(1, -1, 2);
+                    else if (transform.position.y > 0.7f) walk(1, 1, 2);
+                    else arrive = true;
+                    break;
+                default:
+                    if (transform.position.y > -0.2f) walk(1, 1, 2);
+                    else arrive = true;
+                    break;
+            }
+        } 
+    }
+    void waiting() {
+        Invoke("startEating", 3); // 2초뒤 LaunchProjectile함수 호출
+    }
+    void eating() {
+        Invoke("startGoing", 3); // 2초뒤 LaunchProjectile함수 호출
+    }
+    void going() {
+        if (arrive) { standUp(1.0f, catLayer[chairNum - 1]); arrive = false; }
+        else{
+            switch (chairNum) { // start pos = each chair, end pos = 1.6, 2.2
+                case 1:
+                    if (transform.position.y < -0.5f) walk(-1, -1, 21);
+                    else if (transform.position.y < -0.4f) walk(-1, -1, 21);
+                    else if (transform.position.y < -0.1f) walk(-1, 1, 2);
+                    else if (transform.position.y < 1.5f) walk(-1, -1, 2);
+                    else if (transform.position.y < 2.2f) walk(-1, 1, 0);
+                    else Invoke("gone", 0);
+                    break;
+                case 2:
+                    if (transform.position.y > 1.5f) walk(1, -1, 0);
+                    else if (transform.position.y > -0.2f) walk(1, 1, 2);
+                    else if (transform.position.y > -1.4f) walk(1, -1, 21);
+                    else if (transform.position.y > -1.5f) walk(1, 1, 21);
+                    else Invoke("gone", 0);
+                    break;
+                case 3:
+                    if (transform.position.y > 1.5f) walk(1, -1, 0);
+                    else if (transform.position.y > -0.2f) walk(1, 1, 2);
+                    else if (transform.position.y > -1.9f) walk(1, -1, 21);
+                    else if (transform.position.y > -2.2f) walk(1, 1, 31);
+                    else if (transform.position.y > -2.3f) walk(1, -1, 31);
+                    else Invoke("gone", 0);
+                    break;
+                case 4:
+                    if (transform.position.y > 0.6f) walk(1, -1, 0);
+                    else if (transform.position.y > 0f) walk(1, 1, 2);
+                    else if (transform.position.y > -0.6f) walk(1, -1, 11);
+                    else if (transform.position.y > -1.2f) walk(1, 1, 31);
+                    else if (transform.position.y > -1.3f) walk(1, -1, 31);
+                    else Invoke("gone", 0);
+                    break;
+                case 5:
+                    if (forward) {
+                        if (transform.position.y > 0.5f) walk(1, -1, 0);
+                        else if (transform.position.y > -0.1f) walk(1, 1, 2);
+                        else if (transform.position.y > -0.7f) walk(1, -1, 11);
+                        else if (transform.position.y > -0.9f) walk(1, 1, 21);
+                        else if (transform.position.y > -1.6f) walk(1, -1, 21);
+                        else forward = false;
+                    } else if (transform.position.y < -1.5f) walk(-1, -1, 11);
+                    else Invoke("gone", 0);
+                    break;
+                case 6:
+                    if (forward) {
+                        if (transform.position.y > 0.5f) walk(1, -1, 0);
+                        else if (transform.position.y > -0.1f) walk(1, 1, 2);
+                        else if (transform.position.y > -0.5f) walk(1, -1, 11);
+                        else forward = false;
+                    } else if (transform.position.y < -0.4f) walk(-1, -1, 11);
+                    else Invoke("gone", 0);
+                    break;
+                case 7:
+                    if (transform.position.y > 0.5f) walk(1, -1, 0);
+                    else if (transform.position.y > 0f) walk(1, 1, 2);
+                    else if (transform.position.y > -0.2f) walk(1, -1, 2);
+                    else if (transform.position.y > -0.3f) walk(1, 1, 2);
+                    else Invoke("gone", 0);
+                    break;
+                case 8:
+                    if (transform.position.y > 1.5f) walk(1, -1, 0);
+                    else if (transform.position.y > 0.2f) walk(1, 1, 2);
+                    else if (transform.position.y > -0.4f) walk(1, -1, 15);
+                    else Invoke("gone", 0);
                     break;
                 case 9:
                     if (transform.position.y > 1.1f) walk(1, -1, 0);
-                    else if (transform.position.y > 0.7f) walk(1,1, 0);
-                    else drink = true;
+                    else if (transform.position.y > 0.7f) walk(1, 1, 2);
+                    else Invoke("gone", 0);
                     break;
                 default:
-                    if (transform.position.y > -0.2f) walk(1,1, 0);
-                    else drink = true; break;
+                    if (transform.position.y > -0.2f) walk(1, 1, 2);
+                    else Invoke("gone", 0);
+                    break;
             }
-        } else {
-            sitDown(1.0f, catLayer[chairNum-1]);
         }
     }
 
@@ -140,7 +241,20 @@ new SpriteRenderer renderer;
     public void sitDown(float orientation, int layer) { // left = -1, right = 1
         renderer.sortingOrder = layer;
         Vector3 pos = transform.position;
-        pos.x = chairPos[chairNum - 1,0]; pos.y = chairPos[chairNum - 1, 1];
+        pos.x = chairOnPos[chairNum - 1, 0]; pos.y = chairOnPos[chairNum - 1, 1];
         transform.position = pos;
     }
+    public void standUp(float orientation, int layer) { // left = -1, right = 1
+        renderer.sortingOrder = layer;
+        Vector3 pos = transform.position;
+        pos.x = chairOffPos[chairNum - 1, 0]; pos.y = chairOffPos[chairNum - 1, 1];
+        transform.position = pos;
+    }
+    public void startWaiting() { status = 2; }
+    public void startEating() { status = 3; }
+    public void startGoing() {
+        status = 4;
+        animator.SetTrigger("walk_back");
+    }
+    public void gone() { Debug.Log("gone!"); }
 }
